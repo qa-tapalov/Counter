35d402259d019d65adffa8649eadad662a3171ce
Андрей Тапалов <aatapalov@yandex.ru>
2023-10-15 20:07:38 +0400

    создание приложения счётчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var counterUILabel: UILabel!
+    @IBOutlet weak var increaseCount: UIButton!
+    @IBOutlet weak var reduceCountButton: UIButton!
+    @IBOutlet weak var resetCounterButton: UIButton!
+    @IBOutlet weak var textViewMessages: UITextView!
+    
+    var formattedCurrentDate: String {
+        let currentDate = Date()
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yy, HH:mm:ss"
+        return dateFormatter.string(from: currentDate)
     }
-
-
+    
+    var messages: [String] = [] {
+        didSet {
+            textViewMessages.text.append(messages.last ?? "")
+        }
+    }
+    
+    var countToChange = 1 //устанавливается значение на которое будет изменяться counter
+    var counter = 0 {
+        didSet {
+            if counter - oldValue == countToChange {
+                messages.append("[\(formattedCurrentDate)]: значение изменено на +\(countToChange)\n")
+            } else if oldValue - counter == countToChange{
+                messages.append("[\(formattedCurrentDate)]: значение изменено на -\(countToChange)\n")
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        textViewMessages.text = "История изменений:\n\n"
+        textViewMessages.layer.borderWidth = 1
+        textViewMessages.layer.cornerRadius = 10
+        
+        counterUILabel.text = String(counter)
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Actions
+    @IBAction func increaseCount(_ sender: Any) {
+        counter += countToChange
+        counterUILabel.text = String(counter)
+    }
+    
+    @IBAction func reduceCount(_ sender: Any) {
+        if counter > 0 {
+            counter -= countToChange
+            counterUILabel.text = String(counter)
+        } else {
+            messages.append("[\(formattedCurrentDate)]: попытка уменьшить значение счётчика ниже 0\n")
+        }
+         }
+    
+    @IBAction func resetCounter(_ sender: Any) {
+        counter = 0
+        counterUILabel.text = String(counter)
+        messages.append("[\(formattedCurrentDate)]: значение сброшено\n")
+    }
+    
 }
 
